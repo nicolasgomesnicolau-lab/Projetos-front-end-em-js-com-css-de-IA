3942954f7789a0a60a1acf99be205fe3044aa37f
nicolasgomesnicolau-lab <nicolasgomesnicolau@gmail.com>
2026-09-09 16:09:10 -0300

    Update script.js

--- projeto 1/script.js
+++ projeto 1/script.js
@@ -592,7 +592,7 @@ intervalo2 = setInterval(() => {
         if (SelecionaOpcao) {
           SelecionaOpcao = false
           let notesAtivo = false
-          sectionPai.insertAdjacentHTML('beforebegin', inputT);
+          sectionPai.insertAdjacentHTML('afterbegin', inputT);
           const excluirformT = document.querySelector('.topico')
           //excluirformT.remove()
           const formT = document.querySelector('.form-T')
