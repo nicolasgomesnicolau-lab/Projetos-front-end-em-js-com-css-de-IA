b5cf12ae5520cce94fbda6d11f85e6bca5464810
nicolasgomesnicolau-lab <nicolasgomesnicolau@gmail.com>
2026-09-09 15:43:46 -0300

    Update print statement to say 'Goodbye World'

--- projeto 1/script.js
+++ projeto 1/script.js
@@ -592,7 +592,7 @@ intervalo2 = setInterval(() => {
         if (SelecionaOpcao) {
           SelecionaOpcao = false
           let notesAtivo = false
-          sectionPai.insertAdjacentHTML('beforeend', inputT);
+          sectionPai.insertAdjacentHTML('beforebegin', inputT);
           const excluirformT = document.querySelector('.topico')
           //excluirformT.remove()
           const formT = document.querySelector('.form-T')
